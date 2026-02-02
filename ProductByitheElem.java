2c29e7408b5946e9d113704f76307f4905ef7e80
dikshasharma29 <deehaans29@gmail.com>
2026-02-02 21:22:36 +0530

    ProductByithElem.java
    Implement a program to calculate the product of array elements excluding the current element.

--- ProductByitheElem.java
+++ ProductByitheElem.java
@@ -1,3 +1,27 @@
 public class ProductByitheElem {
-    
-}
+    public static void main(String[] args) {
+
+        int[] A = {1, 2, 3, 4,5};
+        int n = A.length;
+
+        int[] P = new int[n];
+        int Product = 1;
+
+        
+        for (int i = 0; i < n; i++) {
+            Product = Product * A[i];
+        }
+
+        
+        for (int i = 0; i < n; i++) {
+            P[i] = Product / A[i];
+        }
+
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(P[i] + " ");
+        }
+    }
+
+
+
