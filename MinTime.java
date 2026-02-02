7ef1a40b92fb7b0980dbd87e1d1ff9d26fbedb5b
dikshasharma29 <deehaans29@gmail.com>
2026-02-02 20:55:44 +0530

    Add files via upload

---
+++ MinTime.java
@@ -0,0 +1,15 @@
+public class MinTime{
+    public static void main(String[] args){
+        int arr[]={2,4,1,3};
+        int max=arr[0];
+        int time=0;
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+           
+        }
+    System.out.println(time);
+   
+    }
+}
